045ec65133cd213d57e6f507fef7c922b9169ca8
alin26 <alin.joseph.shan25@gmail.com>
2026-03-30 11:34:53 +0530

    feat: auth provider with google sign_in and StreamProvider

---
+++ lib/features/auth/providers/auth_provider.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final authStateProvider = StreamProvider<User?>((ref) {
+  return FirebaseAuth.instance.authStateChanges();
+});
+
+class AuthNotifier extends StateNotifier<AsyncValue<void>> {
+  AuthNotifier() : super(const AsyncValue.data(null));
+
+  Future<void> signInWithGoogle() async {
+    try {
+      state = const AsyncValue.loading();
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) {
+        state = const AsyncValue.data(null);
+        return;
+      }
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final credantials = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credantials);
+      state = const AsyncValue.data(null);
+    } catch (e) {
+      state = AsyncValue.error(e, StackTrace.current);
+    }
+  }
+  Future<void> sighnOut() async{
+    await GoogleSignIn().signOut();
+    await FirebaseAuth.instance.signOut();
+  }
+}
+
+final authNorifierProvider =StateNotifierProvider<AuthNotifier,AsyncValue<void>>((ref) {
+  return AuthNotifier();
+},);
