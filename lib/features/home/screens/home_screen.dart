2f523e321ec37731da9d371d23f24fd08884d106
alin26 <alin.joseph.shan25@gmail.com>
2026-03-31 10:11:09 +0530

    feat: auto navigation based on authstate and StreamProvider

---
+++ lib/features/home/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:chat_app/features/auth/providers/auth_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    final authState = ref.watch(authNotifierProvider);
+    return Scaffold(body: Center(child: Text("Home Screen")),floatingActionButton: ElevatedButton(onPressed: () {
+      ref.read(authNotifierProvider.notifier).sighnOut();
+    }, child: Text("Signout")),);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
+import './features/auth/providers/auth_provider.dart';
 import 'features/auth/screens/login_screen.dart';
+import './features/home/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,15 +12,50 @@ void main() async {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateProvider);
     return MaterialApp(
-      title: "Chat App",
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: authState.when(
+        data: (data) => data != null ? HomeScreen() : LoginScreen(),
+        error: (error, stackTrace) =>
+            Scaffold(body: Center(child: Text("Error: $error "))),
+        loading: () => Scaffold(backgroundColor: Color(0xFF3E2C23),
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.chat_bubble_rounded,
+                    size: 90,
+                    color: const Color(0xFF2FA4D7),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Chat App",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFFF5E9D8),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    "Let's connect with world",
+                    style: TextStyle(color: const Color(0xFFF5E9D8)),
+                  ),
+                  const SizedBox(height: 20),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
